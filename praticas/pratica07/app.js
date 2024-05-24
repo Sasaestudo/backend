651baf3d2b0ba48fa0a462077f5174079fda51ae
Sasa <sasaestudo2018@gmail.com>
2024-05-24 17:47:58 -0300

    Adição da pratica07

---
+++ praticas/pratica07/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+// var path = require('path');  :
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+const apidocsRouter = require('./routes/apidocs')
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api-docs', apidocsRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+module.exports = app;
